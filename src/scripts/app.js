4171b146323dbd7e9e74c03c4e7f6077e47a6093
Jefferson Silva <jefessh@outlook.com>
2024-03-21 13:00:17 -0300

    Atualiza funções de criptografia e descriptografia para corresponder ao padrão esperado

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,39 @@
 // Função para criptografar o texto
 function encryptText(text) {
-    // Inverter a string
-    return text.split('').reverse().join('');
+    let encryptedText = [];
+    for (let i = 0; i < text.length; i++) {
+        let letter = text[i];
+        switch (letter) {
+            case "e":
+                encryptedText.push("enter");
+                break;
+            case "i":
+                encryptedText.push("imes");
+                break;
+            case "a":
+                encryptedText.push("ai");
+                break;
+            case "o":
+                encryptedText.push("ober");
+                break;
+            case "u":
+                encryptedText.push("ufat");
+                break;
+            default:
+                encryptedText.push(letter);
+        }
+    }
+    return encryptedText.join('');
 }
 
 // Função para descriptografar o texto
 function decryptText(encryptedText) {
-    // Inverter a string novamente
-    return encryptedText.split('').reverse().join('');
+    encryptedText = encryptedText.replace(/enter/g, 'e');
+    encryptedText = encryptedText.replace(/imes/g, 'i');
+    encryptedText = encryptedText.replace(/ai/g, 'a');
+    encryptedText = encryptedText.replace(/ober/g, 'o');
+    encryptedText = encryptedText.replace(/ufat/g, 'u');
+    return encryptedText;
 }
 
 function setup() {
@@ -17,17 +43,17 @@ function setup() {
     const textInput = document.getElementById('text-input');
     const messageOutput = document.getElementById('message-output');
 
-    encryptButton.addEventListener('click', function() {
+    encryptButton.addEventListener('click', function () {
         const encryptedText = encryptText(textInput.value);
         messageOutput.value = encryptedText;
     });
 
-    decryptButton.addEventListener('click', function() {
+    decryptButton.addEventListener('click', function () {
         const decryptedText = decryptText(messageOutput.value); // Descriptografa o texto atual no campo de saída
         messageOutput.value = decryptedText; // Atualiza o campo de saída com o texto descriptografado
     });
 
-    copyButton.addEventListener('click', function() {
+    copyButton.addEventListener('click', function () {
         messageOutput.select();
         document.execCommand('copy');
         alert('Texto copiado para a área de transferência!');
